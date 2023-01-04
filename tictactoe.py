7fd4cc95e8cdeaa3a45e1ee62a37639e9aa4e87e
Douglas Amaral <douglaxz@hotmail.com>
2023-01-04 09:15:05 -0300

    Melhoria na jogabilidade
    Criado alterancia de players

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,30 @@
-import random
-
+#Declaração de variáveis
 celulas = ['','','','','','','','','']
+continuar = True
+jogador = "X"
+jogadaconcluida = False
 
+
+#Função de criação e atualização do tabuleiro do jogo da velha
 def tabuleiro():
-    
+    print ("| Escolha uma posição conforme o diagrama abaixo: |")
+    print("\n")
+
+    for y in range(0, 9, 3):
+        for x in range(3):      
+            print("|", end="")
+            if(celulas[x+y]==''):
+                print(" ", end="")
+                print(int(x+y)+1, end="")
+                print(" ", end="")
+            else:
+                print(" ", end="")
+                print(" ", end="")
+                print(" ", end="")
+        print("|")
+
+    print("\n")
+
     for y in range(0, 9, 3):
         for x in range(3):      
             print("|", end="")
@@ -21,20 +42,42 @@ def tabuleiro():
 
 
 
-
-print("BEM VINDO AO JOGO DA VELHA")
-print("\n")
-print("| 1 | 2 | 3 |")
-print("| 4 | 5 | 6 |")
-print("| 7 | 8 | 9 |")
-print("\n")
-jogada = input("Jogador X, escolha uma posição conforme o diagrama abaixo:")
-print("\n")
-if (int(jogada) > 0 and int(jogada) < 10):
-    celulas[int(jogada )- 1] = "X"
+while (continuar == True):
+    print("\n")
+    print("--------------------------------------------------------")
+    print("BEM VINDO AO JOGO DA VELHA")
+    print("--------------------------------------------------------")
+    print("\n")
     tabuleiro()
-else:
-    print("Escolha um numero entre 1 e 9")
+
+    jogada = int(input("Jogador "+jogador+" faça sua jogada: "))
+    print("\n")
+    if (jogada > 0 and jogada < 10):
+        if (celulas[jogada- 1])=="":
+            celulas[jogada - 1] = jogador
+            jogadaconcluida = True
+        elif (celulas[jogada- 1])!="":
+            print("Essa celula já está preenchida")    
+            jogadaconcluida = False
+            print("\n")
+        print("\n")
+        tabuleiro()
+        print("\n")
+    else:
+        print("\n")
+        tabuleiro()
+        print("\n")
+        print("Escolha um numero entre 1 e 9")
+        print("\n")
+
+    if(jogadaconcluida == True):
+        if(jogador == "X"):
+            jogador = "O"
+        else:
+            jogador = "X"
+
+    
+    
 
 
 
