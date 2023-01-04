67e16771faaf27efd6fc3be0dcf434606db1d540
Douglas Amaral <douglaxz@hotmail.com>
2023-01-04 10:42:37 -0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,23 @@ celulas = ['','','','','','','','','']
 continuar = True
 jogador = "X"
 jogadaconcluida = False
+rodada = 1
+
+
+#Função reiniciar o jogo
+def reiniciar():
+    global jogador
+    global celulas
+    global continuar
+    global jogadaconcluida
+    global rodada
+    celulas = ['','','','','','','','','']
+    continuar = True
+    jogador = "X"
+    jogadaconcluida = False
+    rodada = 1
+
+
 
 
 #Função para verificar se houve vitoria de algum jogador
@@ -12,10 +29,12 @@ def vitoria(jogador,celulas):
     def celulasPreenchidas(x,y,z,jogador):
         if(celulas[x]==jogador and celulas[y]==jogador and celulas[z]==jogador):
             return(1)
+        else:
+            return(0)
 
 
-    #condições de vitoria do jogador
-    
+    #condições de vitoria do jogador    
+
     #vitoria em linha
     # 1 2 3
     if(vitoria == 0):
@@ -48,13 +67,14 @@ def vitoria(jogador,celulas):
         vitoria = celulasPreenchidas(2,5,8,jogador)    
 
     if(vitoria==1):
-        print("--------------------------------------------------------")
         print("JOGADOR "+jogador+" VENCEU")
         print("--------------------------------------------------------")
-        exit()
-    
-
-
+        resposta = input("Deseja jogar novamente: 'SIM/NÃO'")
+        if(resposta=='não'):
+            exit()
+        else:
+            reiniciar()
+            jogodavelha()
 
 
 
@@ -62,7 +82,6 @@ def vitoria(jogador,celulas):
 def tabuleiro():
     print ("| Escolha uma posição conforme o diagrama abaixo: |")
     print("\n")
-
     for y in range(0, 9, 3):
         for x in range(3):      
             print("|", end="")
@@ -75,9 +94,7 @@ def tabuleiro():
                 print(" ", end="")
                 print(" ", end="")
         print("|")
-
     print("\n")
-
     for y in range(0, 9, 3):
         for x in range(3):      
             print("|", end="")
@@ -91,48 +108,56 @@ def tabuleiro():
                 print(" ", end="")
         print("|")
 
-    
-    
+#Cabeçalho do jogo    
+print("\n")
+print("--------------------------------------------------------")
+print("BEM VINDO AO JOGO DA VELHA")
+print("--------------------------------------------------------")
+print("\n")
+
+#Função do jogo da velha
+def jogodavelha():
+    global rodada
+    global jogador
+    global celulas
+    while (rodada < 10):
+        print("\n")
+        print("--------------------------------------------------------")
+        print("RODADA "+ str(rodada))
+        print("--------------------------------------------------------")
+        vitoria(jogador,celulas)
+        tabuleiro()
         
-
-
-
-
-while (continuar == True):
-    print("\n")
-    print("--------------------------------------------------------")
-    print("BEM VINDO AO JOGO DA VELHA")
-    print("--------------------------------------------------------")
-    print("\n")
-    vitoria(jogador,celulas)
-    tabuleiro()
-    
-    jogada = int(input("Jogador "+jogador+" faça sua jogada: "))
-    print("\n")
-    if (jogada > 0 and jogada < 10):
-        if (celulas[jogada- 1])=="":
-            celulas[jogada - 1] = jogador
-            jogadaconcluida = True
-        elif (celulas[jogada- 1])!="":
-            print("Essa celula já está preenchida")    
-            jogadaconcluida = False
+        jogada = int(input("Jogador '"+jogador+"' faça sua jogada: "))
+        print("\n")
+        if (jogada > 0 and jogada < 10):
+            if (celulas[jogada- 1])=="":
+                celulas[jogada - 1] = jogador
+                jogadaconcluida = True
+            elif (celulas[jogada- 1])!="":
+                print("Essa celula já está preenchida")    
+                jogadaconcluida = False
+                print("\n")
             print("\n")
-        print("\n")
-        tabuleiro()
-        print("\n")
-    else:
-        print("\n")
-        tabuleiro()
-        print("\n")
-        print("Escolha um numero entre 1 e 9")
-        print("\n")
+            tabuleiro()
+            print("\n")
+        else:
+            print("\n")
+            tabuleiro()
+            print("\n")
+            print("Escolha um numero entre 1 e 9")
+            print("\n")
 
-    #if(jogadaconcluida == True):
-    #    if(jogador == "X"):
-    #        jogador = "O"
-    #    else:
-    #        jogador = "X"
+        #função para alterancia de jogadores
+        if(jogadaconcluida == True):
+            if(jogador == "X"):
+                jogador = "O"
+            else:
+                jogador = "X"
+        rodada = int(rodada) + 1
 
+#chamando a função
+jogodavelha()
     
     
 
