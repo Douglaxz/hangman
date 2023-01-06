9b1d6448ade48c6e0032957a4a0c5639f8ca85fd
Douglas Amaral <douglaxz@hotmail.com>
2023-01-06 09:06:15 -0300

    Instalado a opção de novo jogo
    Novo jogo para o jogo da forca

--- forca.py
+++ forca.py
@@ -3,7 +3,7 @@ import random
 #Declaração de váriáveis
 
 palavras = ['abacate','maca', 'tomate','laranja','tangerina','jaca','amendoim']
-
+continuar = True
 
 #desenhar forca
 def desenharForca(erros):
@@ -66,15 +66,26 @@ def desenharForca(erros):
     
     print("\n")
 
-def finalDeJogoPerdeu(palavra):
-    print("|----------------- PERDEU ! -----------------|")
+
+def jogarNovamente():
+    resposta = ""
+    while(resposta==""):
+        resposta = input("Deseja jogar novamente: 'S/N'")
+        resposta = resposta.upper()
+        if(resposta=='N'):
+            exit()
+        elif (resposta=='S'):
+            jogoforca()
+        else:
+            print("Resposta inválida")
+            resposta = ""
+    print("--------------------------------------------------------")
+
+def finalDeJogo(msg, palavra):
+    print("|----------------- "+msg.upper()+" ! -----------------|")
     print("A PALAVRA SECRETA ERA: "+ palavra.upper())
     print("\n")
 
-def finalDeJogoGanhou(palavra):
-    print("|----------------- VENCEU ! -----------------|")
-    print("A PALAVRA SECRETA ERA: "+ palavra.upper())
-    print("\n")
 
 #funcao forca
 def jogoforca():
@@ -94,55 +105,58 @@ def jogoforca():
     
     print("\n")
     
-    while(erros < 6):
-        #chamada da função de desenho da forca
-        desenharForca(erros)
-
-        #criação do placar
-        print("|----------------- PLACAR -----------------|")
-        print("|    ACERTOS     | ", end="")
-        for letra in letrasCorretas:
-            print(letra, end="")
-            print(" ", end="")
-        print("\n")
-        print("| LETRAS ERRADAS | ", end="")
-        for letra in letrasErradas:
-            print(letra, end="")
-            print(" ", end="")        
-        print("\n")        
-        print("|      ERROS     | ", end="")
-        print(erros)
-        print("\n")     
-
-        #pedir pro usuario uma letra
-        letra = (input("Digite uma letra: "))
-        
-
-        #verificar se a letra consta na palavra
-        contador = 0
-        if(letra in palavra):
-            for p in palavra:
-                if(p == letra):
-                    letrasCorretas[contador] = letra.upper()
-                contador = contador + 1
-        else:
-            if(letra not in letrasErradas):
-                letrasErradas.append(letra.upper())
-            erros = erros + 1
-        print("\n")
-        
-        #verificar se houve vitoria
-        novapalavra = ''.join(letrasCorretas)
-        
-        if(novapalavra == palavra.upper()):
-            finalDeJogoGanhou(palavra)
-            exit()
-
-
-        #verificar se houve derrota
-        if(erros==6):
+    while(continuar == True):
+        while(erros < 6):
+            #chamada da função de desenho da forca
             desenharForca(erros)
-            finalDeJogoPerdeu(palavra)
+
+            #criação do placar
+            print("|----------------- PLACAR -----------------|")
+            print("|    ACERTOS     | ", end="")
+            for letra in letrasCorretas:
+                print(letra, end="")
+                print(" ", end="")
+            print("\n")
+            print("| LETRAS ERRADAS | ", end="")
+            for letra in letrasErradas:
+                print(letra, end="")
+                print(" ", end="")        
+            print("\n")        
+            print("|      ERROS     | ", end="")
+            print(erros)
+            print("\n")     
+
+            #pedir pro usuario uma letra
+            letra = (input("Digite uma letra: "))
+            
+
+            #verificar se a letra consta na palavra
+            contador = 0
+            if(letra in palavra):
+                for p in palavra:
+                    if(p == letra):
+                        letrasCorretas[contador] = letra.upper()
+                    contador = contador + 1
+            else:
+                if(letra not in letrasErradas):
+                    letrasErradas.append(letra.upper())
+                erros = erros + 1
+            print("\n")
+            
+            #verificar se houve vitoria
+            novapalavra = ''.join(letrasCorretas)
+            
+            if(novapalavra == palavra.upper()):
+                finalDeJogo("ganhou", palavra)
+                jogarNovamente()
+
+
+
+            #verificar se houve derrota
+            if(erros==6):
+                desenharForca(erros)
+                finalDeJogo("perdeu", palavra)
+                jogarNovamente()
 
 
 
